c240a16580beb3d942982cf8c89b4e040706150e
4lex11 <95388653+4lex11@users.noreply.github.com>
2026-07-08 22:22:56 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/WokHome' : '',
 }
 
-export default nextConfig
+module.exports = nextConfig
