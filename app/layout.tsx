7cc466d01470c1c5e06a96f2b60b177b7e96a227
4lex11 <95388653+4lex11@users.noreply.github.com>
2026-07-08 23:33:40 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,15 @@ export default function RootLayout({
   return (
     <html lang="es" className="bg-background">
       <head>
+        <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-GYG4M205P5"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-GYG4M205P5');
+          </script>
         {/* Google Tag Manager */}
         <Script
           id="gtm-script"
